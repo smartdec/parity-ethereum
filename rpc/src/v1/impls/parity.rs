136fe74edb60b1b6e9c52c81090546ddc5c29c02
Seun LanLege <seunlanlege@gmail.com>
2018-09-17 10:11:12 +0100

    refactored tests for better readability moved verify_signature tests to live in the same file.

--- rpc/src/v1/impls/parity.rs
+++ rpc/src/v1/impls/parity.rs
@@ -40,12 +40,12 @@ use v1::helpers::{self, errors, fake_sign, ipfs, SigningQueue, SignerService, Ne
 use v1::metadata::Metadata;
 use v1::traits::Parity;
 use v1::types::{
-	Bytes, U256, H64, H160, H256, H512, CallRequest,
+	Bytes, U256, H64, U64, H160, H256, H512, CallRequest,
 	Peers, Transaction, RpcSettings, Histogram,
 	TransactionStats, LocalTransactionStatus,
 	BlockNumber, ConsensusCapability, VersionInfo,
 	OperationsInfo, ChainStatus,
-	AccountInfo, HwAccountInfo, RichHeader, Receipt, RichBasicAccount,
+	AccountInfo, HwAccountInfo, RichHeader, Receipt, BasicAccount,
 	block_number_to_id
 };
 use Host;
@@ -482,7 +482,7 @@ impl<C, M, U, S> Parity for ParityClient<C, M, U> where
 		helpers::submit_work_detail(&self.client, &self.miner, nonce, pow_hash, mix_hash)
 	}
 
-	fn verify_signature(&self, is_prefixed: bool, message: Bytes, r: H256, s: H256, v: u64) -> Result<RichBasicAccount> {
+	fn verify_signature(&self, is_prefixed: bool, message: Bytes, r: H256, s: H256, v: U64) -> Result<BasicAccount> {
 		verify_signature(is_prefixed, message, v, r, s, self.client.signing_chain_id())
 	}
 }
